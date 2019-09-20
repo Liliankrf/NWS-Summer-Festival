78dfa33824060e2c8d0df334aba3966e03223395
Francois-Xavier <xavierdk@outlook.fr>
2019-09-20 16:31:23 +0200

    js generation div avec button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,47 @@
-console.log("reussis");
+
+let boutonCharger = document.querySelector('#plus button'); 
+boutonCharger.addEventListener('click', chargerPlus)
 
 
+async function chargerPlus(){
+    console.log('je charge...');
+
+    let aAjouter = await chargerJSON('js/etudiant.json');
+
+    
+    for(let i = 0; i < aAjouter.length; i++){
+        console.log(ajouterHTML);
+        ajouterHTML(aAjouter[i]);
+    }
+}
+
+
+async function chargerJSON(fichier) {
+    let response = await fetch(fichier);
+    let etudiant = await response.json();
+    return etudiant;
+}
+
+function ajouterHTML(etudiant){
+
+    let lesEtudiants = document.querySelector('#container');
+    let bouton = document.querySelector('#plus');
+    
+    console.log(etudiant.Nom);
+ 
+   let article = document.createElement('article');
+   article.classList.add('divetudiant' , etudiant.Prénom)
+
+   let titre = document.createElement('h3');
+   titre.innerHTML = `Specialité ${etudiant.Qualité1} - ${etudiant.Qualité2}`; // Ajoutons ici le titre récupéré
+   
+   let p = document.createElement('p');
+   p.innerHTML = etudiant.Nom + ' ' + etudiant.Prénom;
+
+   article.appendChild(titre);
+   article.appendChild(p);
+   lesEtudiants.insertBefore(article, bouton);
+}
 let i = 0;
 function ani(){
     i++;
@@ -44,6 +85,7 @@ if(h=1){
 }
 
 window.onload = test;
+
 function scrollDown() {
     window.scrollTo(0, 1000);    
 }
