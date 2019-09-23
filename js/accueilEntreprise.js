320000bc0f05574f29cf9f9c0b033d3bc2d5e262
Francois-Xavier <xavierdk@outlook.fr>
2019-09-23 02:35:59 +0200

    Accueil Entreprise Géneration
    L'index.html redirige bien vers la page "accueilEntreprise"
    
    connexion entre le .json et le js faite
    
    j'ai crée un block etudiant sur un html / css vierge pour ensuite le stylisé comme le demande les designers, ensuite je l'ai préparé a une génération automatique(1)  de ce dernier en le placant au centre etc.. { (1)en fonction du nombre élèves inscrit sur le .json}
    j'ai fais en sorte que la géneration ce passe bien sur le accueilEntreprise.html
    
    ne toucher pas au js de la génération svp je vais expliquer demain si vous voulez mieux comprendre comme récup les variables pour la page profile etc..
    
    bug d'animation page start résolu

--- js/accueilEntreprise.js
+++ js/accueilEntreprise.js
@@ -1,6 +1,14 @@
+let compteur = 0;
+function test()
+{
+compteur++;
+if(h=1){
+    setTimeout(function() { document.getElementById('logostart').style.display = 'none';chargerPlus()}, 3500);      
+} 
+}
 
 
-/*
+
 async function chargerPlus(){
     console.log('je charge...');
 
@@ -77,45 +85,5 @@ function ajouterHTML(etudiant){
    lesEtudiants.insertBefore(containerCard, bouton);
 }
 
-*/
-let i = 0;
-function ani(){
-    i++;
-    if(i%2 === 0){
-        document.getElementById('containerConnexion').style.display = 'none';
-    }
-    else{
-    document.getElementById('containerConnexion').style.display = 'block';
-    }
-    if(i%2 === 0 && j & 1)
-    {
-        console.log("reussis");
-        document.getElementById('inscriptionEtudiant').style.display = 'none';
-    }
-    console.log("la valeur de i est : " + i);
-    console.log("la valeur de j est : " + j);
-    
-}
-let j = 0;
-function ani2(){
-    j++;
-    if(j%2 === 0){
-        document.getElementById('inscriptionEtudiant').style.display = 'none';
-    }
-    else{
-    document.getElementById('inscriptionEtudiant').style.display = 'block';
-    }
-    console.log("la valeur de j est : " + j);
-}
-
-let compteur = 0;
-function test()
-{
-compteur++;
-if(h=1){
-    setTimeout(function() { document.getElementById('logostart').style.display = 'none'; 
-    document.getElementById('ContainerAccueil').style.display = 'block';chargerPlus()}, 3500);      
-} 
-}
 
 window.onload = test;
